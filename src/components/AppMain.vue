0812a72189886091e07bab0455942fc12197f16b
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-26 16:16:19 +0200

    fix workcolture

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,10 @@
 <script>
 import Jumbotron from '../components/Jumbotron.vue';
+import WorkColture from '../components/WorkColture.vue'
 export default {
     components: {
-        Jumbotron
+        Jumbotron,
+        WorkColture
     },
     name: "AppMain"
 }
@@ -11,51 +13,8 @@ export default {
 
 <template>
     <Jumbotron></Jumbotron>
-    <div class="workColture">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="textWork">
-                        <h6>EVERYTHING IN <strong>MAXCOACH</strong></h6>
-                        <div class="textWorkBody">
-                            <h2>Learn about our <br> <strong class="coulture">Work Colture</strong> at<br> MaxCoach</h2>
-                            <p>Spend some time to visit our website or head office and <br> discover our current courses,
-                                enrollment procedure, and, <br> registration deadline, We're opening new classes every <br>
-                                beginning of each month.</p>
-                            <p>Have questions? <strong class="coulture2">Get Free Guide &#129046;</strong></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <img class="imgPositionTop"
-                        src="../assets/img/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="">
-                    <img class="imgPosition" src="../assets/img/about-us-01-ohkia3qg98rq9vg3s0mn2hkflr1migdqdw370qnegc.jpg"
-                        alt="">
-                </div>
-            </div>
-            <div class="numberDate">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <div class="dateBody">
-                                <p> 129+</p>
-                                <h5><strong class="dateCoulture">ENROLLED LEARNERES</strong></h5>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <p>80</p>
-                            <h5><strong class="dateCoulture">FINISHED SESSIONS</strong></h5>
-                        </div>
-                        <div class="col">
-                            <p>4%</p>
-                            <h5><strong class="dateCoulture">SATISFACTIONS RATE</strong></h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
+    <WorkColture></WorkColture>
+   
 </template>
 
 
---
+++ src/components/WorkColture.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    name: 'WorkColture'
+}
+</script>
+
+<template>
+    <div class="workColture">
+        <div class="container">
+            <div class="row py-5">
+                <div class="col">
+                    <div class="textWork">
+                        <h6>EVERYTHING IN <strong>MAXCOACH</strong></h6>
+                        <div class="textWorkBody">
+                            <h2>Learn about our <br> <strong class="coulture">Work Colture</strong> at<br> MaxCoach</h2>
+                            <p>Spend some time to visit our website or head office and <br> discover our current courses,
+                                enrollment procedure, and, <br> registration deadline, We're opening new classes every <br>
+                                beginning of each month.</p>
+                            <p>Have questions? <strong class="coulture2">Get Free Guide &#129046;</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <img class="imgPositionTop"
+                        src="../assets/img/stock-full-hd-10-ohki1ayywq3cqutettrncduujpoaorifqu4relt2ts.jpg" alt="">
+                    <img class="imgPosition" src="../assets/img/about-us-01-ohkia3qg98rq9vg3s0mn2hkflr1migdqdw370qnegc.jpg"
+                        alt="">
+                        <img class="circle" src="../assets/img/maxcoach-shape-02.png" alt="">
+                </div>
+            </div>
+            <div class="numberDate">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="dateBody">
+                                <p> 129+</p>
+                                <h5><strong class="dateCoulture">ENROLLED LEARNERES</strong></h5>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <p>80</p>
+                            <h5><strong class="dateCoulture">FINISHED SESSIONS</strong></h5>
+                        </div>
+                        <div class="col">
+                            <p>4%</p>
+                            <h5><strong class="dateCoulture">SATISFACTIONS RATE</strong></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style></style>
