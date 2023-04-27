2ab584e1a186df3338df7ef2b59a8aebd66b3a11
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-27 14:45:05 +0200

    Add AppFooter

--- src/components/EnterEmail.vue
+++ src/components/EnterEmail.vue
@@ -7,7 +7,7 @@ export default {
 
 <template>
     <div class="email py-5">
-        <div class="titleEmail text-center">
+        <div class="titleEmail text-center py-3">
             <h2><strong>Subscrive</strong><strong class="coulture">Newsletters</strong></h2>
             <p>Enter your email to register to our newsletters subscription <br> delivered on a regular basis!</p>
         </div>
@@ -16,10 +16,6 @@ export default {
             <button type="button" class="rounded-end btnEmail">Subscrive</button>
         </div>
     </div>
-
-
-
-
 </template>
 
 
